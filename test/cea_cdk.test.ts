2325fc828277600dee2c7c3ce5b0d5ec26a01114
kenny864 <kenny864@gmail.com>
2026-06-06 17:20:54 -0400

    Initial commit

---
+++ test/cea_cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CeaCdk from '../lib/cea_cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cea_cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CeaCdk.CeaCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
